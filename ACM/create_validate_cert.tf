9158cad14af9a51bd17a1eba639ee502c8368ade
Samuel Montes <dodgers88blue@gmail.com>
2020-03-09 21:10:17 -0700

    Create create_validate_cert.tf
    creates ACM certificate then validates it

---
+++ ACM/create_validate_cert.tf
@@ -0,0 +1,30 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "example.com"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "dev"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_route53_zone" "zone" {
+  name         = "example.com."
+  private_zone = false
+}
+
+resource "aws_route53_record" "cert_validation" {
+  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.zone.id
+  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+}
